5d529fe834a0d445eda388b44c569e46d1cf2688
farkasarnold1981@gmail.com <farkasarnold1981@gmail.com@379da728-e24b-3fdc-10af-dc58055a7fc8>
2013-11-18 05:48:55 +0000

    multiple camel routes

--- trunk/PhotoOrderingTool/src/main/java/hu/arnoldfarkas/pot/service/camel/CamelMovingService.java
+++ trunk/PhotoOrderingTool/src/main/java/hu/arnoldfarkas/pot/service/camel/CamelMovingService.java
@@ -3,11 +3,9 @@ package hu.arnoldfarkas.pot.service.camel;
 import hu.arnoldfarkas.pot.service.MovingService;
 import java.io.File;
 import java.io.FileFilter;
-import java.util.logging.Level;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
@@ -18,17 +16,13 @@ import org.springframework.stereotype.Service;
 public class CamelMovingService implements MovingService, InitializingBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CamelMovingService.class);
+    
+    @Autowired
     private RouteBuilder routeBuilder;
-    private CamelContext context;
-    @Autowired
-    private FtpConfig ftpConfig;
-    @Autowired
-    private DropboxConfig dropboxConfig;
+    private final CamelContext context = new DefaultCamelContext();
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        routeBuilder = new MovingFromFolderRouteBuilder(ftpConfig, dropboxConfig);
-        context = new DefaultCamelContext();
         try {
             LOGGER.debug("Adding route...");
             context.addRoutes(routeBuilder);
@@ -65,11 +59,11 @@ public class CamelMovingService implements MovingService, InitializingBean {
 
     private boolean isFinised() {
         final File[] listFiles = MovingFromFolderRouteBuilder.FROM_FOLDER.listFiles(new FileFilter() {
-                                     @Override
-                                     public boolean accept(File f) {
-                                         return f.isFile();
-                                     }
-                                 });
+            @Override
+            public boolean accept(File f) {
+                return f.isFile();
+            }
+        });
         return listFiles == null || listFiles.length < 1;
     }
 
--- trunk/PhotoOrderingTool/src/main/java/hu/arnoldfarkas/pot/service/camel/MovingFromFolderRouteBuilder.java
+++ trunk/PhotoOrderingTool/src/main/java/hu/arnoldfarkas/pot/service/camel/MovingFromFolderRouteBuilder.java
@@ -1,70 +1,41 @@
 package hu.arnoldfarkas.pot.service.camel;
 
 import java.io.File;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.RouteDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
+@Component
 public class MovingFromFolderRouteBuilder extends RouteBuilder {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MovingFromFolderRouteBuilder.class);
     public static final File FROM_FOLDER = new File("temp/upload/");
-    private final FtpConfig ftpConfig;
-    private final DropboxConfig dropboxConfig;
 
-    public MovingFromFolderRouteBuilder(FtpConfig ftpConfig, DropboxConfig dropboxConfig) {
-        this.ftpConfig = ftpConfig;
-        this.dropboxConfig = dropboxConfig;
-    }
-
+    @Value("#{camel['routes']}")
+    private String routes;
+    
     @Override
     public void configure() throws Exception {
-        String fileFrom = getFromFolder();
-        LOGGER.debug("Folder2FtpRouteBuilder From: {}", fileFrom);
-        LOGGER.debug("Folder2FtpRouteBuilder To: {}", getToFtp());
-
-        from(fileFrom).process(new Processor() {
-            @Override
-            public void process(Exchange exchng) throws Exception {
-                File f = exchng.getIn().getBody(File.class);
-                LOGGER.debug("Folder2FtpRouteBuilder processing: " + f.getName());
-            }
-        })
-          .to(getToFtp())
-          .to(getToDropbox());
+        String[] camelRoutes = getCamelRoutes();
+        Assert.notEmpty(camelRoutes);
+        
+        RouteDefinition routeDefinition = from(getFromFolder());
+        for (String route : camelRoutes) {
+            routeDefinition = routeDefinition.to(route);
+        }
     }
 
     private String getFromFolder() {
         return "file://" + FROM_FOLDER.getAbsolutePath();
     }
 
-    private String getToFtp() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(ftpConfig.getType());
-        sb.append("://");
-        sb.append(ftpConfig.getUsername());
-        sb.append("@");
-        sb.append(ftpConfig.getHost());
-        sb.append(":");
-        sb.append(ftpConfig.getPort());
-        sb.append(ftpConfig.getPath());
-        sb.append("?password=");
-        sb.append(ftpConfig.getPassword());
-        return sb.toString();
-    }
-
-    private String getToDropbox() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("dropbox");
-        sb.append("://");
-        sb.append("?");
-        sb.append("userId=");
-        sb.append(dropboxConfig.getUserId());
-        sb.append("&accessToken=");
-        sb.append(dropboxConfig.getAccessToken());
-        return sb.toString();
+    private String[] getCamelRoutes() {
+        Assert.notNull(routes);
+        return routes.split(",");
     }
 
 }
